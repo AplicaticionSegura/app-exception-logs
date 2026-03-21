68448d0c363ebdf29a413f06dbc08d0e27656a1e
AplicaticionSegura <lberrospirua@gmail.com>
2026-03-20 19:29:16 -0500

    proyecto base

---
+++ src/main/java/pe/edu/idat/app_exception_logs/AppExceptionLogsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_exception_logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExceptionLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppExceptionLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_exception_logs/AppExceptionLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_exception_logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExceptionLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
