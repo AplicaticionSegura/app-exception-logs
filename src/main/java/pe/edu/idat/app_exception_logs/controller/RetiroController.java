0b50f64da39ae40f3d0194f0efc10529f5ef3c3f
AplicaticionSegura <lberrospirua@gmail.com>
2026-03-27 20:02:21 -0500

    proyecto terminado

--- src/main/java/pe/edu/idat/app_exception_logs/controller/RetiroController.java
+++ src/main/java/pe/edu/idat/app_exception_logs/controller/RetiroController.java
@@ -1,12 +1,19 @@
 package pe.edu.idat.app_exception_logs.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pe.edu.idat.app_exception_logs.dto.RetiroForm;
 import pe.edu.idat.app_exception_logs.service.RetiroService;
+
+import javax.naming.Binding;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,4 +27,25 @@ public class RetiroController {
         model.addAttribute("retiroForm", new RetiroForm());
         return "retiro-form";
     }
+
+    @PostMapping("/procesar")
+    public String procesarRetiro(@Valid @ModelAttribute("retiroForm") RetiroForm retiroForm,
+                                 BindingResult result,
+                                 Model model,
+                                 HttpServletRequest request){
+        if(result.hasErrors()){
+            return "retiro-form";
+        }
+        String usuario = "operador01";
+        String ipOrigen = request.getRemoteAddr();
+        retiroService.realizarRetiro(
+                retiroForm.getClienteId(),
+                retiroForm.getMonto(),
+                usuario, ipOrigen);
+        model.addAttribute("mensaje",
+                "Retiro realizado correctamente");
+        return "retiro-exito";
+
+    }
+
 }
